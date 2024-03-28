745cfafb8876cc5161f79cf22761bfef5f40df44
opoet <i@opoet.cn>
2024-03-28 15:57:12 +0800

    update

---
+++ src/components/ui/md-button.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { createComponent } from "@lit/react";
+import { MdTextButton } from "@material/web/button/text-button";
+import { MdIconButton } from "@material/web/iconbutton/icon-button"
+import { MdElevatedButton } from "@material/web/button/elevated-button";
+
+export const TextButton = createComponent({
+  tagName: 'md-text-button',
+  elementClass: MdTextButton,
+  react: React,
+  events: {
+    onClick: 'click',
+  },
+});
+
+export const IconButton = createComponent({
+  tagName: 'md-icon-button',
+  elementClass: MdIconButton,
+  react: React,
+  events: {
+    onClick: 'click',
+  },
+});
+
+export const ElevatedButton = createComponent({
+  tagName: 'md-elevated-button',
+  elementClass: MdElevatedButton,
+  react: React,
+  events: {
+    onClick: 'click',
+  },
+});
+
