ce5fd42bd5e57a9c9a35961e2cf41dbfbf393449
PzHown <1263097055@qq.com>
2024-09-14 11:20:04 +0800

    v0.0.3

--- src/components/page_components/index-page-3-com.tsx
+++ src/components/page_components/index-page-3-com.tsx
@@ -1,6 +1,6 @@
 export default function Icons() {
     return (
-        <div className="absolute top-0 grid grid-cols-4 md:grid-cols-6 mx-auto gap-4 h-240 overflow-hidden w-full justify-items-center items-center">
+        <div className="index_page_3-box absolute top-0 grid grid-cols-4 mx-auto h-240 w-full items-center justify-items-center gap-4 overflow-hidden md:grid-cols-6">
             {Array.from({ length: 48 }).map((_, index) => (
                 <Icon key={index} />
             ))}
@@ -9,24 +9,24 @@ export default function Icons() {
 }
 
 function Icon() {
-    const randomColor = getRandomColor();
-    const randomBorderRadius = getRandomBorderRadius();
+    const randomColor = getRandomColor()
+    const randomBorderRadius = getRandomBorderRadius()
     return (
-        <div className="size-12 shadow-md" style={{ backgroundColor: randomColor, borderRadius: randomBorderRadius }}>
-            <div className="size-full  bg-white bg-op-20 scale-75 transform-origin-c" style={{ borderRadius: randomBorderRadius }} />
+        <div className="index_page_3-icon size-12 shadow-sm md:(size-18)" style={{ backgroundColor: randomColor, borderRadius: randomBorderRadius }}>
+            <div className="size-full transform-origin-c scale-75 bg-white bg-op-20" style={{ borderRadius: randomBorderRadius }} />
         </div>
-    );
+    )
 }
 
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
+    const letters = '0123456789ABCDEF'
+    let color = '#'
     for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)]
     }
-    return color;
+    return color
 }
 
 function getRandomBorderRadius() {
-    return Math.random() > 0.5 ? '0.25rem' : '50%';
+    return Math.random() > 0.5 ? '0.25rem' : '50%'
 }
--- src/tools/animation.ts
+++ src/tools/animation.ts
@@ -9,11 +9,11 @@ interface AnimateFunction {
  * 观察元素并为其子元素执行动画。
  *
  * @param {Element} element - 要观察的元素。
- * @param {NodeListOf<HTMLParagraphElement>} items - 要应用动画的子元素集合。
- * @param {(item: Element, index: number) => void} animateFn - 动画函数，接受子元素和索引作为参数。
+ * @param {NodeListOf<Element> | undefined} items - 要应用动画的子元素集合。
+ * @param {(item: Element | null, index: number) => void} animateFn - 动画函数，接受子元素和索引作为参数。
  * @param {boolean} isOnce - 是否只执行一次动画。如果为 true，动画执行完毕后取消观察。
  */
-export function observeAndAnimateForList(element: Element, items: NodeListOf<HTMLParagraphElement>, animateFn: AnimateFunction, isOnce: boolean) {
+export function observeAndAnimateForList(element: Element | null, items: NodeListOf<Element> | undefined, animateFn: AnimateFunction, isOnce: boolean) {
   if (element && items) {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
