e5978365dec5da9133e2d069fe7cd0adae38732d
PzHown <1263097055@qq.com>
2024-09-20 09:33:50 +0800

    Add MiSans font and Oblatum 3.0 SVG icon

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -105,7 +105,7 @@ interface IconPreviewProps {
 
 export function IconPreview({ children }: IconPreviewProps) {
   return (
-    <div className="relative grid col-span-full grid-cols-3 cursor-pointer gap-6 rounded-4 from-[rgba(41,170,224,0.1)] to-[rgba(108,128,208,0.2)] bg-gradient-to-rb p-4 shadow-sm">
+    <div className="relative grid col-span-full grid-cols-3 cursor-pointer gap-6 rounded-4 from-[rgba(41,170,224,0.1)] to-[rgba(108,128,208,0.2)] bg-gradient-to-rb p-4 shadow-sm md:py-8">
       <Ripple />
       {children}
     </div>
