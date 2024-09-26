a69ea16d05e7f7dcf0c48a68648b456b7c243f6a
PzHown <1263097055@qq.com>
2024-09-26 16:09:31 +0800

    Refactor home-card component and add new cards

--- src/components/home-card.tsx
+++ src/components/home-card.tsx
@@ -1,4 +1,7 @@
 import { type ReactNode, useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import AlipayImg from '../../public/icons/oblatum/alipay.png'
 import { FilledButton, TextButton } from './ui/md-button'
 import { Ripple } from './ui/md-ripple'
 
@@ -10,7 +13,7 @@ interface baseProps {
 
 export function HomeCard({ className, children, title }: baseProps) {
   return (
-    <div className={`${className} h-full w-full border-1 border-gray border-op-40 rounded-4 bg-[var(--md-sys-color-inverse-on-surface)] p-6 md:p-8 [&>h1]:(font-bold text-op-40 text-white text-sm)`}>
+    <div className={`${className} h-full w-full border-1 border-gray border-op-20 rounded-4 bg-[var(--md-sys-color-surface)] p-6 md:p-8 [&>h1]:(font-bold text-op-40 text-white text-sm)`}>
       {title && <h1 className="mb-4">{title}</h1>}
       {children}
     </div>
@@ -19,18 +22,23 @@ export function HomeCard({ className, children, title }: baseProps) {
 
 export function NewVersonCard() {
   const [version, setVersion] = useState('0.0.0')
-  const [whatsNew, setWhatsNew] = useState([])
+  const [whatsNew, setWhatsNew] = useState('')
   async function getVerson() {
-    const response = await fetch('https://api.github.com/repos/Oblatum/Oblatum-IconPack-Reborn/releases')
-    const text = await response.json()
+    fetch('http://raw.oblatum.art/releases.json')
+      .then(response => response.json())
+      .then((data) => {
+        setVersion(data[0].tag_name)
+        setWhatsNew(data[0].body.split('## What\'s Changed\r\n')[1])
+      })
+      .catch((err) => {
+        console.error('Fetch error:', err)
+      })
     // //如果text[0]不存在，说明没有release，直接返回
     // if (!text[0]) {
     //     state.value += -90
     //     return
     // }
     // 截取versionName开头，换行符结尾的字符串
-    setVersion(text[0].tag_name)
-    setWhatsNew(text[0].body.split('## What\'s Changed\r\n')[1].split('\n'))
     // update_time.value = new Date(text[0].assets[0].updated_at).toLocaleString()
     // setDownload_url(text[0].assets[0].browser_download_url)
     // download_count.value = text[0].assets[0].download_count
@@ -48,13 +56,11 @@ export function NewVersonCard() {
           <br className="md:hidden" />
           现已推送！
         </h1>
-        <div className="rounded-2 bg-[var(--md-sys-color-on-secondary)] p-4 md:mb-4 [&>p]:(text-sm)">
-          {whatsNew.map((item, index) => {
-            return <p key={index}>{`${item}`}</p>
-          })}
+        <div className="rounded-2 bg-[var(--md-sys-color-on-secondary)] px-8 py-6 md:mb-4 [&>*]:(text-sm text-[var(--md-sys-color-inverse-surface)])">
+          <Markdown remarkPlugins={[remarkGfm]}>{whatsNew}</Markdown>
         </div>
         <div className="flex gap-4">
-          <FilledButton onClick={() => { window.open('/apks/com.oblatum.iconpack.apk') }}>下载APK</FilledButton>
+          <FilledButton onClick={() => { window.open('http://raw.oblatum.art/oblatum_iconpack_latest.apk') }}>下载APK</FilledButton>
           <TextButton onClick={() => { window.open('https://github.com/Oblatum/Oblatum-IconPack-Reborn/commit/master') }}>查看更新详情</TextButton>
         </div>
       </div>
@@ -84,7 +90,7 @@ export function DevoloperCard() {
     <HomeCard title="开发者">
       <div className="grid grid-cols-3 gap-4">
         {devolopers.map(devoloper => (
-          <div onClick={() => { window.open(devoloper.github) }} key={devoloper.name} className="relative flex flex-col cursor-pointer items-center gap-2 rounded-4 bg-[var(--md-sys-color-surface-variant)] px-2 py-4">
+          <div onClick={() => { window.open(devoloper.github) }} key={devoloper.name} className="relative flex flex-col cursor-pointer items-center gap-2 rounded-4 bg-[var(--md-sys-color-inverse-on-surface)] px-2 py-4">
             <Ripple />
             <img src={devoloper.avatar} alt={devoloper.name} className="size-12 rounded-full" />
             <h1 className="text-sm font-bold">{devoloper.name}</h1>
@@ -102,7 +108,7 @@ export function PyCard() {
   return (
     <HomeCard title="PY小店">
       <div className="flex flex-col items-center gap-4">
-        <img src="../../public/icons/oblatum/alipay.png" alt="alipay" className="w-24" />
+        <img src={AlipayImg.src} alt="alipay" className="w-24" />
         <p>暂未开放，敬请期待...</p>
       </div>
     </HomeCard>
